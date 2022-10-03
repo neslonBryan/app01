251c3b9ad4f7fd1b8926855bd491c13e02e2b3b9
Nelson Bryan <201913582B@utea.edu.pe>
2022-10-03 13:33:53 -0500

    comit de card

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.grey,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
@@ -19,23 +19,71 @@ class MyApp extends StatelessWidget {
               radius: 70.0,
               backgroundColor: Colors.white,
               backgroundImage: NetworkImage(
-                  "https://images.pexels.com/photos/447329/pexels-photo-447329.jpeg"),
+                  "https://www.anmosugoi.com/wp-content/uploads/2021/01/Kanojo-Okarishimasu-MAMI.jpg"),
             ),
             Text(
-              "Fiorela de Fátima Guadalupe",
+              "Mami God ",
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 25.0,
+                fontSize: 40.0,
                 fontWeight: FontWeight.bold,
+                fontFamily: "Dancing",
               ),
             ),
             Text(
-              "FLUTTER DEVELOPER",
+              "Porfavor, Pisame la cara",
               style: TextStyle(
-                color: Colors.grey,
+                color: Colors.black,
                 fontWeight: FontWeight.w500,
-                fontSize: 20.0,
+                fontSize: 30.0,
                 letterSpacing: 2.0,
+              ),
+            ),
+            Icon(Icons.heart_broken, size: 40, color: Colors.red),
+            Divider(
+              thickness: 1.0,
+              color: Colors.black,
+              indent: 80,
+              endIndent: 80,
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 10.0,
+              ),
+              elevation: 10,
+              child: ListTile(
+                title: Text("+51 **********"),
+                subtitle: Text("Numero Privado"),
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.blue,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 10.0,
+              ),
+              elevation: 10,
+              child: ListTile(
+                title: Text("nelsonquispecarpio@gmail.com"),
+                subtitle: Text("Correo Electronico"),
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.blue,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.red,
+                ),
               ),
             ),
           ],
