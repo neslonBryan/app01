51ab21d91efe2179399c0cca86cef3a560308153
Nelson Bryan <201913582B@utea.edu.pe>
2022-10-03 12:12:22 -0500

    Agregado textos

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,29 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.indigo,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: const [
             CircleAvatar(
               radius: 70.0,
               backgroundColor: Colors.white,
               backgroundImage: NetworkImage(
                   "https://images.pexels.com/photos/447329/pexels-photo-447329.jpeg"),
+            ),
+            Text(
+              "Fiorela de Fátima Guadalupe",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w500,
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),
             ),
           ],
         ),
